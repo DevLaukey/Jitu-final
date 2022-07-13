4c0cdfc931cccc8aafcfca48a152f940a5028a68
Benson <bensonkimkam@gmail.com>
2022-07-13 16:43:41 +0300

    swiper slider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import ProductGrid from "./components/products/productGrid";
+import Slider from "./components/Slider/Slider";
 function App() {
   return (
     <div className="relative">
       <Navbar />
+      <Slider />
       <ProductGrid />
-
       <Footer />
     </div>
   );
